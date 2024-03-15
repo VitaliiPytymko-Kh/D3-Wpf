1cd3a0c3d4cd8365ab5c1f53566505e580c42afe
VitaliiPytymko <vitaliipytymko@gmail.com>
2024-03-15 21:50:09 +0200

    переделываю под MVVM

---
+++ WpfApp1/Model/MediaPlayerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class MediaPlayerModel
+    {
+        public string MediaFilePath { get; set; }
+        public double Volume { get; set; }
+        public TimeSpan Position { get; set; }
+        public TimeSpan Duration { get; set; }
+        public bool IsPlaying { get; set; }
+    }
+}
---
+++ WpfApp1/ViewModel/MainWindowViewModel .cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private MediaPlayerModel _player;
+
+        public MainWindowViewModel()
+        {
+            _player = new MediaPlayerModel();
+            PlayCommand = new RelayCommand(Play);
+            PauseCommand = new RelayCommand(Pause);
+            StopCommand = new RelayCommand(Stop);
+            OpenCommand = new RelayCommand(Open);
+        }
+        public string MediaFilePath
+        {
+            get { return _player.MediaFilePath; }
+            set
+            {
+                _player.MediaFilePath = value;
+                OnPropertyChanged(nameof(MediaFilePath));
+            }
+        }
+        public TimeSpan Position
+        {
+            get { return _player.Position; }
+            set
+            {
+                _player.Position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+        public TimeSpan Duration
+        {
+            get { return _player.Duration; }
+            set
+            {
+                _player.Duration = value;
+                OnPropertyChanged(nameof(Duration));
+            }
+        }
+        public bool IsPlaying
+        {
+            get { return _player.IsPlaying; }
+            set
+            {
+                _player.IsPlaying = value;
+                OnPropertyChanged(nameof(IsPlaying));
+            }
+        }
+
+        public ICommand PlayCommand { get; set; } 
+        public ICommand PauseCommand { get; set; }
+        public ICommand StopCommand { get; set; }
+        public ICommand OpenCommand { get; set; }
+
+        private void Play(object parametr)
+        {
+            
+        }
+        private void Pause(object parametr) { }
+        private void Stop(object parametr) { }
+        private void Open(object parametr) { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
